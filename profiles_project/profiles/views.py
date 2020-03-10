74d21987d547295be225779772f71cd028355599
DraghiciStefan1984 <draghicistefan1984@gmail.com>
2020-03-10 08:21:55 +0200

    profiles api

--- profiles_project/profiles/views.py
+++ profiles_project/profiles/views.py
@@ -1,7 +1,9 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
-from profiles import serializers
+from rest_framework import status, viewsets, filters
+from rest_framework.authentication import TokenAuthentication
+from profiles import serializers, models, permissions
+
 
 # Create your views here.
 class HelloApiView(APIView):
@@ -17,3 +19,49 @@ class HelloApiView(APIView):
             return Response({'message':message})
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk=None):
+        return Response({'method':'PUT'})
+
+    def patch(self, request, pk=None):
+        return Response({'method':'PATCH'})
+
+    def patch(self, request, pk=None):
+        return Response({'method':'DELETE'})
+
+
+class HelloViewSet(viewsets.ViewSet):
+    def list(self, request):
+        viewset=['this is a test viewset']
+        return Response({'message':'Hello', 'viewset':viewset})
+
+    def create(self, request):
+        serializer=self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name=serializer.validated_data.get('name')
+            message=f'Hello {name}'
+            return Response({'message':message})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk=None):
+        return Response({'http_method':'GET'})
+
+    def update(self, request, pk=None):
+        return Response({'http_method':'PUT'})
+
+    def partial_update(self, request, pk=None):
+        return Response({'http_method':'PATCH'})
+
+    def destroy(self, request, pk=None):
+        return Response({'http_method':'DELETE'})
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    serializer_class=serializers.UserProfileSerializer
+    queryset=models.UserProfile.objects.all()
+    authentication_classes=(TokenAuthentication,)
+    permission_classes=(permissions.UpdateOwnProfile,)
+    filter_backends=(filters.SearchFilter,)
+    search_fields=('name', 'email')
