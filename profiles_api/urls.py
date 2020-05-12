b3c30524c4c8a11fd232238b6d4089d296699bc5
draghicistefan <draghicistefan1984@gmail.com>
2020-05-12 08:18:20 +0300

    small updates

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,7 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from rest_framework.routers import DefaultRouter
 
+router=DefaultRouter()
+router.register('hello-viewset', views.HelloViewSet, basename='hello-viewset')
 
 urlpatterns = [
     path('hello/', views.HelloApiView.as_view()),
+    path('', include(router.urls)),
 ]
