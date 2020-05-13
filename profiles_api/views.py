6a5ad4f198b5dc1e38f5bfe703429cb9e1c21ba4
draghicistefan <draghicistefan1984@gmail.com>
2020-05-13 10:30:11 +0300

    added login

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -3,6 +3,8 @@ from rest_framework.response import Response
 from rest_framework import status, viewsets, filters
 from . import serializers, models, permissions
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 
 class HelloApiView(APIView):
@@ -33,6 +35,7 @@ class HelloApiView(APIView):
 
     def delete(self, request, pk=None):
         return Response({'method': 'DELETE'})
+
 
 
 class HelloViewSet(viewsets.ViewSet):
@@ -72,3 +75,8 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     permission_classes=(permissions.UpdateOwnProfile,)
     filter_backends=(filters.SearchFilter,)
     search_fields=('name', 'email')
+
+
+
+class UserLoginApiView(ObtainAuthToken):
+    renderer_classes=api_settings.DEFAULT_RENDERER_CLASSES
