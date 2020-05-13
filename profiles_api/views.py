39696f2daf44d6095df5570f0ceafce23bf9ed30
DraghiciStefan1984 <draghicistefan1984@gmail.com>
2020-05-13 09:31:12 +0300

    added search

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,7 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status, viewsets
-from . import serializers
+from rest_framework import status, viewsets, filters
+from . import serializers, models, permissions
+from rest_framework.authentication import TokenAuthentication
 
 
 class HelloApiView(APIView):
@@ -61,3 +62,13 @@ class HelloViewSet(viewsets.ViewSet):
 
     def destroy(self, request, pk=None):
         return Response({'http_method':'DELETE'})
+
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    serializer_class=serializers.UserProfileSerializer
+    queryset=models.UserProfile.objects.all()
+    authentication_classes=(TokenAuthentication,)
+    permission_classes=(permissions.UpdateOwnProfile,)
+    filter_backends=(filters.SearchFilter,)
+    search_fields=('name', 'email')
